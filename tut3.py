404b792350026c638c2868aa20336ab5b0619e66
Abhi76318 <avinashabhishek052@gmail.com>
2025-08-01 12:59:45 +0530

    Add files via upload

---
+++ tut3.py
@@ -0,0 +1,52 @@
+"""
+Iteration
+while <condition>:
+    <code>
+    <code> 
+
+......For loop.......
+
+for <variable> in <sequence of values> :
+    <code>
+
+for n in range (5):
+    print(n)
+
+range(start, stop, step) --> same as slicing
+"""
+
+# count=0
+# where = input("Go left or right? ")
+
+# while where == "right" :
+#     count+=1
+#     if count>2 :
+#         print("😔")
+#     where = input ("Go left or right? ")
+   
+# print ("You got out!")
+
+'''......Factorial.....'''
+
+# x=4
+# i=1
+# fac=1
+# while i<=4 :
+#     fac*=i
+#     i+=1
+# print(f'{x} factorial is {fac}')
+
+#Practice Qs
+
+# mysum = 0
+# start = 2
+# end = 6
+# for i in range(start, end +1) :
+#     mysum+= i
+# print (mysum)
+
+N = 5
+i=1
+while i<=N :
+    print("hello world")
+    i += 1
