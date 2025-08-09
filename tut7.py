cd7d5ea1dc90d5bed901e43186b60744af7cbeba
Abhi76318 <avinashabhishek052@gmail.com>
2025-08-09 23:03:16 +0530

    Add files via upload

---
+++ tut7.py
@@ -0,0 +1,35 @@
+"""Decomposition, Abstraction, Functions"""
+
+'''
+-->Decomposition - Breaking down a large, complex problem into smaller,
+   more managable sub problems. Achieved through Functions, Classes and
+   objects, modules and packages.
+-->Abstraction - Hiding Implementation detials 
+-->Functions - Organize codes into reusable blocks 
+   characteristics include 
+   >Has a Name
+   >Has Parameters
+   >Has Docstring(Specification /comment)
+   >Has a body
+   >returns something
+'''
+#Functions Syntax
+# def is_even(i):
+#     """
+#     Input: i, a positive int
+#     Returns True if i is even, otherwise False
+#     """
+#     if i%2 == 0:
+#         return True
+#     else:
+#         return False
+# or return i%2 == 0 (only)-same
+#invoke a fuction by
+#--> is_even(3)
+# ---> Entire Function call is replaced by return
+
+# def div_by(n,d):
+#     return n%d==0
+
+# print(div_by(10,3))
+# print(div_by(195,13))
